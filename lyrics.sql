99696cf6ec0f427b89368a8420f2e7d014a9a06e
tanweichin <75352114+tanweichin@users.noreply.github.com>
2021-02-03 01:54:31 +0800

    Add files via upload

--- lyrics.sql
+++ lyrics.sql
@@ -6,7 +6,7 @@ INSERT INTO lyricstable (lyrics_id, lyrics_no, lyrics_song, lyrics_status, lyric
 			('105', '5', 'River Flows In You', 'Completed', 'songlyrics5.pdf'),
 			('106', '6', 'Photograph', 'Completed', 'songlyrics6.pdf'),
 			('107', '7', 'Love Story', 'Completed', 'songlyrics7.pdf'),
-			('108', '8', 'Negaraku', 'Completed', 'songlyrics8.pdf');
+			('108', '8', 'Negaraku', 'Completed', 'songlyrics8.pdf'),
 			('109', '9', 'Kimi Ni Todoke', 'Completed', 'songlyrics9.pdf'),
 			('110', '10', 'Where You Are', 'Completed', 'songlyrics10.pdf'),
 			('111', '11', 'Everything is Alright', 'Completed', 'songlyrics11.pdf'),	
